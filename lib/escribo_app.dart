adc3da4afcee71e45e59cd8f6b669f9283d2facd
Pedro Siqueira <pedro.siqueira1999@hotmail.com>
2023-12-08 09:11:19 -0300

    first commit

---
+++ lib/escribo_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:librady_app/features/library/library_screen.dart';
+
+class EscriboApp extends StatelessWidget {
+  const EscriboApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LibraryScreen(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:librady_app/escribo_app.dart';
+
+void main() {
+  runApp(const EscriboApp());
+}
+
